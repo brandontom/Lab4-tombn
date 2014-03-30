8a8b639b2f3a0dc559471767d30a02245c02e811
Brandon Tom <tombn@rose-hulman.edu>
2014-03-29 21:18:36 -0400

    returns empty list for 1

---
+++ Lab4-tombn/src/PrimeNumber.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class PrimeNumber {
+
+	public static ArrayList<Integer> generateFactors(int i) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		return result;
+	}
+	
+}
