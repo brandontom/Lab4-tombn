a358a449741d8bf0a97ca85d2a92593a78c8e7c2
Brandon Tom <tombn@rose-hulman.edu>
2014-03-29 21:29:19 -0400

    prime number generation generates empty list for 1

---
+++ Lab4-tombn/src/PrimeNumberGenerator.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class PrimeNumberGenerator {
+
+	
+	public static ArrayList<Integer> generateFactors(int i) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if (i <= 1)
+			return result;
+		
+		return result;
+	}
+	
+}
---
+++ Lab4-tombn/src/PrimeNumberGeneratorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class PrimeNumberGeneratorTest {
+
+	@Test
+	public void testifTrueisTrue() {
+		assertTrue("assertTrue failed..", true);
+	}
+	
+	@Test
+	public void testOne() {
+		assertEquals(makeList(), PrimeNumber.generateFactors(1));
+	}
+
+	
+	public ArrayList<Integer> makeList(int... ints) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(int i : ints) {
+			result.add(i);
+		}
+		return result;
+	}
+}
